8154c741620080fcecc777842c55f3475334292d
Jeffrey Gordon <jbgordon00@gmail.com>
2024-05-25 23:41:42 -0400

    Refactor connector arrows

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
 import './App.css';
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import SettingsBar from './components/SettingsBar';
 import Sequencer from './components/Sequencer';
 import EnergyPiano from './components/EnergyPiano';
 import MidiHandler from './components/MidiHandler';
-
-function ConnectorArrow({ startX, startY, endX, endY, arrowDirection }) {
-    const bumpX = arrowDirection === 'right' ? startX - 40 : startX + 40;
-    const radius = 20;
-    const cornerX = arrowDirection === 'right' ? bumpX + radius : bumpX - radius;
-    const lineEndX = arrowDirection === 'right' ? endX - 20 : endX + 20;
-
-    const pathD = `
-        M${startX},${startY} 
-        L${cornerX},${startY} 
-        A${radius},${radius} 0 0 ${arrowDirection === 'left' ? 1 : 0} ${bumpX},${startY + radius} 
-        L${bumpX},${endY - radius} 
-        A${radius},${radius} 0 0 ${arrowDirection === 'left' ? 1 : 0} ${cornerX},${endY} 
-        L${lineEndX},${endY}
-    `;
-
-    const arrowX = arrowDirection === 'right' ? endX - 20 : endX + 20;
-    const points = `${endX},${endY} ${arrowX},${endY + 20} ${arrowX},${endY - 20}`;
-
-    const style = {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%'
-    }
-
-    return (
-        <svg style={style}>
-            <path d={pathD} stroke='var(--dark-grey)' strokeWidth="10" fill='none' />
-            <polygon points={points} fill='var(--dark-grey)' />
-        </svg>
-    )
-}
+import ConnectorArrow from './components/ConnectorArrow';
 
 
 export default function ProbabilisticMidiSequencer() {
@@ -46,6 +13,10 @@ export default function ProbabilisticMidiSequencer() {
     const [energies, setEnergies] = useState(Array.from({ length: 16 }, () => (new Map())));
     const [activeStep, setActiveStep] = useState(0); // The currently "playing" step
     const [selectedStep, setSelectedStep] = useState(0); // The step for which the user is editing energies
+
+    const settingsBarRef = useRef(null);
+    const sequencerRef = useRef(null);
+    const energyPianoRef = useRef(null);
 
     function setSelectedStepEnergies(selectedStepEnergies) {
         const nextEnergies = energies.slice();
@@ -61,52 +32,18 @@ export default function ProbabilisticMidiSequencer() {
         setIsPlaying(prevValue => !prevValue);
     }
 
-    const settingsBarRef = useRef(null);
-    const sequencerRef = useRef(null);
-    const energyPianoRef = useRef(null);
-
-    // Hide these off canvas by default
-    const [settingsSequencerConnector, setSettingsSequencerConnector] = useState({
-        startX: -2000,
-        startY: -2000,
-        endX: -2000,
-        endY: -2000,
-        arrowDirection: 'right'
-    });
-    const [sequencerPianoConnector, setSequencerPianoConnector] = useState({
-        startX: -2000,
-        startY: -2000,
-        endX: -2000,
-        endY: -2000,
-        arrowDirection: 'left'
-    });
-
-    useEffect(() => {
-        const settingsBarRect = settingsBarRef.current.getBoundingClientRect();
-        const sequencerRect = sequencerRef.current.getBoundingClientRect();
-        const energyPianoRect = energyPianoRef.current.getBoundingClientRect();
-
-        setSettingsSequencerConnector({
-            startX: settingsBarRect.left,
-            startY: settingsBarRect.top + 0.5 * settingsBarRect.height,
-            endX: sequencerRect.left,
-            endY: sequencerRect.top + sequencerRect.height * 0.125,
-            arrowDirection: 'right'
-        });
-
-        setSequencerPianoConnector({
-            startX: sequencerRect.right,
-            startY: sequencerRect.bottom - 0.125 * sequencerRect.height,
-            endX: energyPianoRect.right,
-            endY: energyPianoRect.top + energyPianoRect.height * 0.3,
-            arrowDirection: 'left'
-        });
-    }, []);
-
     return (
         <>
-            <ConnectorArrow {...settingsSequencerConnector} />
-            <ConnectorArrow {...sequencerPianoConnector} />
+            <ConnectorArrow
+                startRef={settingsBarRef}
+                endRef={sequencerRef}
+                arrowDirection='right'
+            />
+            <ConnectorArrow
+                startRef={sequencerRef}
+                endRef={energyPianoRef}
+                arrowDirection='left'
+            />
             <div className='d-flex align-items-center vh-100 background light-grey-background'>
                 <div className="container text-center" style={{ maxWidth: '800px' }}>
                     <div className='row card' ref={settingsBarRef}>
---
+++ src/components/ConnectorArrow.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+export default function ConnectorArrow({ startRef, endRef, arrowDirection }) {
+    const [path, setPath] = useState({
+        startX: -2000,
+        startY: -2000,
+        endX: -2000,
+        endY: -2000
+    });
+
+    useEffect(() => {
+        const updatePath = () => {
+            const startRect = startRef.current.getBoundingClientRect();
+            const endRect = endRef.current.getBoundingClientRect();
+            const startX = (arrowDirection === 'right' ? startRect.left : startRect.right);
+            const startY = startRect.bottom - 50;
+            const endX = (arrowDirection === 'right' ? endRect.left : endRect.right);
+            const endY = endRect.top + 50;
+
+            setPath({
+                startX: startX,
+                startY: startY,
+                endX: endX,
+                endY: endY
+            });
+        }
+
+        updatePath();
+        window.addEventListener('resize', updatePath);
+
+        return () => { window.removeEventListener('resize', updatePath); }
+    }, [startRef, endRef, arrowDirection])
+
+    const bumpX = arrowDirection === 'right' ? path.startX - 40 : path.startX + 40;
+    const radius = 20;
+    const cornerX = arrowDirection === 'right' ? bumpX + radius : bumpX - radius;
+    const lineEndX = arrowDirection === 'right' ? path.endX - 20 : path.endX + 20;
+
+    const pathD = `
+        M${path.startX},${path.startY} 
+        L${cornerX},${path.startY} 
+        A${radius},${radius} 0 0 ${arrowDirection === 'left' ? 1 : 0} ${bumpX},${path.startY + radius} 
+        L${bumpX},${path.endY - radius} 
+        A${radius},${radius} 0 0 ${arrowDirection === 'left' ? 1 : 0} ${cornerX},${path.endY} 
+        L${lineEndX},${path.endY}
+    `;
+
+    const arrowX = arrowDirection === 'right' ? path.endX - 20 : path.endX + 20;
+    const points = `${path.endX},${path.endY} ${arrowX},${path.endY + 20} ${arrowX},${path.endY - 20}`;
+
+    const style = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+    }
+
+    return (
+        <svg style={style}>
+            <path d={pathD} stroke='var(--dark-grey)' strokeWidth="10" fill='none' />
+            <polygon points={points} fill='var(--dark-grey)' />
+        </svg>
+    )
+}
